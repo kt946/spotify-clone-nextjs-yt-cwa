b21d5bd2fc385e63dd787b61b50a7d24eab20ea0
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-06-17 10:33:49 -0400

    added Sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import './globals.css'
-import { Figtree } from 'next/font/google'
+import './globals.css';
+import { Figtree } from 'next/font/google';
+import Sidebar from '@/components/Sidebar';
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Spotify Clone',
   description: 'Listen to music!',
-}
+};
 
 export default function RootLayout({
   children,
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
     </html>
-  )
+  );
 }
