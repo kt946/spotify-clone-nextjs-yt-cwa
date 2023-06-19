a4ce2aa474d80a33cb8f3448d017e52d78838b40
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-06-19 18:06:36 -0400

    added stripe package and types.ts, created SupabaseProvider and UserProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import { Figtree } from 'next/font/google';
 import Sidebar from '@/components/Sidebar';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -9,17 +11,17 @@ export const metadata = {
   description: 'Listen to music!',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
