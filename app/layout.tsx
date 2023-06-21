6d1d465eae774fae731057b22383b0546e40915c
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-06-21 14:48:08 -0400

    added Modal, ModalProvider and useAuthModal hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Figtree } from 'next/font/google';
 import Sidebar from '@/components/Sidebar';
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import UserProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={font.className}>
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>
               {children}
             </Sidebar>
