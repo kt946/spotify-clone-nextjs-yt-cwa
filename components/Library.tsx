23dc90f86071684ab19733c3dfae465fa66d025a
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-06-26 14:52:27 -0400

    updated Sidebar component to fetch song data

--- components/Library.tsx
+++ components/Library.tsx
@@ -6,8 +6,15 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
+import { Song } from '@/types';
 
-const Library = () => {
+import MediaItem from './MediaItem';
+
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -37,7 +44,15 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem
+            onClick={() => {}}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,12 +7,14 @@ import { BiSearch } from 'react-icons/bi';
 import Box from './Box';
 import SidebarItem from './SidebarItem';
 import Library from './Library';
+import { Song } from '@/types';
 
 interface SidebarProps {
   children: React.ReactNode;
+  songs: Song[];
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
   const routes = useMemo(
     () => [
@@ -36,19 +38,20 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     <div className="flex h-full">
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
         <Box>
-          <div className='flex flex-col gap-y-4 px-5 py-4'>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
             {routes.map((item) => (
-              <SidebarItem key={item.label} {...item}/>
+              <SidebarItem
+                key={item.label}
+                {...item}
+              />
             ))}
           </div>
         </Box>
-        <Box className='overflow-y-auto h-full'>
-          <Library />
+        <Box className="overflow-y-auto h-full">
+          <Library songs={songs} />
         </Box>
       </div>
-      <main className='h-full flex-1 overflow-y-auto py-2'>
-        {children}
-      </main>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
