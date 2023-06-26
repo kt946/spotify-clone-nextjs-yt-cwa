769fd6158e21598e9073ec5291bfd9d7230bac30
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-06-26 14:21:48 -0400

    added SongItem component to main page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'vdziffinxopykuyoazwk.supabase.co'
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
