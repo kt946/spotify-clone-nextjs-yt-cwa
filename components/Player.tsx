2e10bf62daa52115e5bbbdfc27e2ab9c1e712cf1
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-06-29 12:27:07 -0400

    added song player controls and volume slider

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import useGetSongById from '@/hooks/useGetSongById';
 import useLoadSongUrl from '@/hooks/useLoadSongUrl';
 import usePlayer from '@/hooks/usePlayer';
+import PlayerContent from './PlayerContent';
 
 const Player = () => {
   const player = usePlayer();
@@ -12,7 +13,15 @@ const Player = () => {
 
   if (!song || !songUrl || !player.activeId) return null;
 
-  return <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">Player</div>;
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      <PlayerContent
+        key={songUrl}
+        song={song}
+        songUrl={songUrl}
+      />
+    </div>
+  );
 };
 
 export default Player;
