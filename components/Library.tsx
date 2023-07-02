1f8b9c1f36cb8c4c15d2d5f4813f2b6d63c38198
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-07-02 19:58:53 -0400

    added SubscribeModal

--- components/Library.tsx
+++ components/Library.tsx
@@ -8,6 +8,7 @@ import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
 import { Song } from '@/types';
 import useOnPlay from '@/hooks/useOnPlay';
+import useSubscribeModal from '@/hooks/useSubscribeModal';
 
 import MediaItem from './MediaItem';
 
@@ -16,9 +17,10 @@ interface LibraryProps {
 }
 
 const Library: React.FC<LibraryProps> = ({ songs }) => {
+  const subscribeModal = useSubscribeModal();
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
-  const { user } = useUser();
+  const { user, subscription } = useUser();
 
   const onPlay = useOnPlay(songs);
 
@@ -27,7 +29,9 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       return authModal.onOpen();
     }
 
-    // TODO: Check for subscription
+    if (!subscription) {
+      return subscribeModal.onOpen();
+    }
 
     return uploadModal.onOpen();
   };
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -3,21 +3,30 @@
 import { useEffect, useState } from 'react';
 
 import AuthModal from '@/components/AuthModal';
+import SubscribeModal from '@/components/SubscribeModal';
 import UploadModal from '@/components/UploadModal';
+import { ProductWithPrice } from '@/types';
 
-const ModalProvider = () => {
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
-  if (!isMounted) return null;
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <>
       <AuthModal />
       <UploadModal />
+      <SubscribeModal products={products} />
     </>
   );
 };
