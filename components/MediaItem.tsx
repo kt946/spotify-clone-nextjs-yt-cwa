d41c43f5aa14cd4b6e252cdc4baa94bdcaee0d52
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-07-02 20:11:36 -0400

    updated MediaItem component

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 
 import useLoadImage from '@/hooks/useLoadImage';
 import { Song } from '@/types';
+import usePlayer from '@/hooks/usePlayer';
 
 interface MediaItemProps {
   data: Song;
@@ -11,6 +12,7 @@ interface MediaItemProps {
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+  const player = usePlayer();
   const imageUrl = useLoadImage(data);
 
   const handleClick = () => {
@@ -18,7 +20,7 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
       return onClick(data.id);
     }
 
-    // TODO: Default turn on player
+    return player.setId(data.id);
   };
 
   return (
